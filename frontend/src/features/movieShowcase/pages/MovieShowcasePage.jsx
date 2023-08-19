79af9964553425c20f302788c4f8e97c23ce7383
ipjayawick <ipjayawick@gmail.com>
2023-08-19 15:44:38 +0530

    movie showcase done

--- frontend/src/features/movieShowcase/pages/MovieShowcasePage.jsx
+++ frontend/src/features/movieShowcase/pages/MovieShowcasePage.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import MovieDetails from '../components/MovieDetails';
 
 function MovieShowcasePage() {
   return (
-    <div>MovieShowcasePage</div>
+    <>
+      <Container fluid className='p-0'>
+        <div >
+          <iframe
+            title="YouTube Video"
+            className="ratio ratio-4x3"
+            src="https://www.youtube.com/embed/uYPbbksJxIg"
+            allowFullScreen
+            style={{ height: '60vh' }}
+          ></iframe>
+        </div>
+
+      </Container>
+      <MovieDetails/>
+    </>
   )
 }
 
