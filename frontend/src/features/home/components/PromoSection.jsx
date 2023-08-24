3bd98c53f451e57dcffb1f7a2971c4df953db44a
ipjayawick <ipjayawick@gmail.com>
2023-08-24 17:37:35 +0530

    promo section done

--- frontend/src/features/home/components/PromoSection.jsx
+++ frontend/src/features/home/components/PromoSection.jsx
@@ -5,8 +5,8 @@ import PromotionCard from './PromotionCard'
 function PromoSection() {
     return (
         <div>
-            <Container fluid className="bg-light" style={{ padding: "100px" }}>
-                <h2 className="my-5">Deals and Exclusive</h2>
+            <Container fluid className="bg-light pb-5" style={{paddingLeft:"100px",paddingRight:"100px"}}>
+                <h2 className="py-5">Deals and Exclusive</h2>
                 <PromotionCard />
             </Container>
         </div>
