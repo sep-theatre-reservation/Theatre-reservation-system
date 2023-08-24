e859d18b0b4184ba2a62c9a76c4d72304a6eddbb
Anushna Jayathunga <116999649+Anushna99@users.noreply.github.com>
2023-08-24 14:06:19 +0530

    remote pulled

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useCallback, useState } from "react"
-import { AuthContext } from './features/shared/context/auth-context'
-import NavbarComponent from './features/shared/components/NavbarComponent'
-import HomePage from './features/home/pages/HomePage'
-import NowShowing from './features/movies/components/NowShowing'
-import CommingSoon from './features/movies/components/CommingSoon'
-import MovieShowcasePage from './features/movieShowcase/pages/MovieShowcasePage'
-import MoviesPage from './features/movies/pages/MoviesPage'
-import Footer from './features/shared/components/Footer'
-import BookingPage from './features/booking/pages/bookingPage';
-import PaymentPage from './features/payment/pages/PaymentPage'
-import DashboardPage from './features/admin/pages/DashboardPage'
-import AdminPage from "./features/admin/pages/AdminPage";
+import { useCallback, useState } from "react";
+import { AuthContext } from "./features/shared/context/auth-context";
+import NavbarComponent from "./features/shared/components/NavbarComponent";
+import HomePage from "./features/home/pages/HomePage";
+import NowShowing from "./features/movies/components/NowShowing";
+import CommingSoon from "./features/movies/components/CommingSoon";
+import MovieShowcasePage from "./features/movieShowcase/pages/MovieShowcasePage";
+import MoviesPage from "./features/movies/pages/MoviesPage";
+import Footer from "./features/shared/components/Footer";
+import BookingPage from "./features/booking/pages/bookingPage";
+import PaymentPage from "./features/payment/pages/PaymentPage";
+import DashboardPage from "./features/admin/pages/DashboardPage";
 import SeatSelection from "./features/reservations/pages/SeatSelection";
 import TheatreManagePage from "./features/admin/pages/TheatreManagePage";
 
@@ -43,12 +42,12 @@ function App() {
         <NavbarComponent />
         <main style={{ minHeight: "80vh" }}>
           <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path="/admin/*" element={<DashboardPage/>} >
-              <Route index element={<DashboardPage/>}></Route>
-              <Route path='movies' element={<DashboardPage/>}></Route>
-              <Route path='theatres' element={<TheatreManagePage/>}></Route>
-              <Route path='promo' element={<DashboardPage/>}></Route>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/admin/*" element={<DashboardPage />}>
+              <Route index element={<DashboardPage />}></Route>
+              <Route path="movies" element={<DashboardPage />}></Route>
+              <Route path="theatres" element={<TheatreManagePage />}></Route>
+              <Route path="promo" element={<DashboardPage />}></Route>
             </Route>
             <Route path="/seats" element={<SeatSelection />} />
             <Route path="/movies/*" element={<MoviesPage />}>
