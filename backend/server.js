074bee879321504c84767f9c8d58e44a9e7640e4
Anushna Jayathunga <116999649+Anushna99@users.noreply.github.com>
2023-08-31 13:41:21 +0530

    theatre removal bug fixed

--- backend/server.js
+++ backend/server.js
@@ -36,20 +36,10 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occured!.." });
 });
 
-// const DB_USERNAME = "Anushna";
-// const DB_PASSWORD = "6n5cAEtqbgeOeDP9";
-// const DB_CLUSTER = "cluster0";
-// const DB_NAME = "sep";
-
-// const uri = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-
-// , {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }
-
 const uri =
-  "mongodb+srv://Anushna:6n5cAEtqbgeOeDP9@cluster0.1cddrqz.mongodb.net/sep?retryWrites=true&w=majority";
+  "mongodb+srv://ashashipriya:ne7rAGwdHsicdr91@cluster0.zkwkqs7.mongodb.net/?retryWrites=true&w=majority";
+
+//const uri = "mongodb+srv://Anushna:6n5cAEtqbgeOeDP9@cluster0.1cddrqz.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose
   .connect(uri)
