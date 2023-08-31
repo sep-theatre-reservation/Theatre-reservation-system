3fd54ca1030207237cfc3dce4aeda1b83f2742bf
Anushna Jayathunga <116999649+Anushna99@users.noreply.github.com>
2023-09-01 03:56:54 +0530

    movies page and movieShowCase  connected to backend

--- frontend/src/features/movieShowcase/pages/MovieShowcasePage.jsx
+++ frontend/src/features/movieShowcase/pages/MovieShowcasePage.jsx
@@ -1,12 +1,36 @@
+import { useParams } from "react-router-dom";
 import MovieDetails from "../components/MovieDetails";
 import MovieTrailerSection from "../components/MovieTrailerSection";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import React, { useEffect, useState } from "react";
+import LoadingOverlay from "../../shared/components/LoadingOverlay";
 
 function MovieShowcasePage() {
+  const { isLoading, sendRequest } = useHttpClient();
+  const [loadedMovie, setLoadedMovie] = useState();
+  const movieId = useParams().movieId;
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:3000/api/movies/${movieId}`
+        );
+        setLoadedMovie(responseData.movie);
+      } catch (err) {
+        /* */
+      }
+    };
+
+    fetchMovies();
+  }, [sendRequest, movieId]);
+
   return (
-    <>
-      <MovieTrailerSection />
-      <MovieDetails />
-    </>
+    <React.Fragment>
+      {isLoading && <LoadingOverlay asOverlay />}
+      {!isLoading && loadedMovie && <MovieTrailerSection movie={loadedMovie} />}
+      {!isLoading && loadedMovie && <MovieDetails movie={loadedMovie} />}
+    </React.Fragment>
   );
 }
 
