61e004b621a7c7854f6f422fab00ef8f625c6f8a
ipjayawick <ipjayawick@gmail.com>
2023-09-04 22:02:54 +0530

    minor changed

--- frontend/src/features/home/components/MoviesSection.jsx
+++ frontend/src/features/home/components/MoviesSection.jsx
@@ -3,39 +3,38 @@ import { useHttpClient } from "../../shared/hooks/http-hook";
 import Container from "react-bootstrap/Container";
 import LoadingOverlay from "../../shared/components/LoadingOverlay";
 import MoviesGrid from "../../shared/components/MoviesGrid";
+import "../components/bg.css"
 function MoviesSection() {
-    const { isLoading, sendRequest } = useHttpClient();
-    const [movieList, setMovieList] = useState([]);
-  
-    useEffect(() => {
-      const fetchMovies = async () => {
-        try {
-          const responseData = await sendRequest(
-            "http://localhost:3000/api/movies"
-          );
-          setMovieList(responseData.movies);
-        } catch (err) {
-          /* */
-        }
-      };
-      fetchMovies();
-    }, [sendRequest]);
+  const { isLoading, sendRequest } = useHttpClient();
+  const [movieList, setMovieList] = useState([]);
 
-    return (
-        <>
-            {isLoading && <LoadingOverlay asOverlay />}
-            <div>
-                <Container
-                    fluid
-                    className="bg-light"
-                    style={{ paddingLeft: "100px", paddingRight: "100px" }}
-                >
-                    <h2 className="py-5">Movies</h2>
-                <MoviesGrid movieList={movieList}/>
-                </Container>
-            </div>
-        </>
-    )
+  useEffect(() => { fetchMovies(); }, [sendRequest]);
+
+  const fetchMovies = async () => {
+    try {
+      const responseData = await sendRequest(
+        "http://localhost:3000/api/movies"
+      );
+      setMovieList(responseData.movies);
+    } catch (err) {
+      /* */
+    }
+  };
+
+  return (
+    <>
+      {isLoading && <LoadingOverlay asOverlay />}
+      <div className="custom-background">
+        <Container
+          fluid
+          style={{ paddingLeft: "100px", paddingRight: "100px" }}
+        >
+          <h2 className="py-5">Movies</h2>
+          <MoviesGrid movieList={movieList} />
+        </Container>
+      </div>
+    </>
+  )
 }
 
 export default MoviesSection
--- frontend/src/features/home/components/PromoSection.jsx
+++ frontend/src/features/home/components/PromoSection.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
 import PromotionCard from './PromotionCard'
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function PromoSection() {
+    const [pormotionList, setPromotionList] = useState([]);
+    const { isLoading, sendRequest } = useHttpClient();
+
+    useEffect(() => { fetchPromotions() }, [sendRequest])
+
+    const fetchPromotions = async () => {
+        try {
+            const responseData = await sendRequest(
+                "http://localhost:3000/api/promotions"
+            )
+            setPromotionList(responseData.promotions)
+        } catch (error) {
+
+        }
+    }
+
     return (
         <div>
-            <Container fluid className="bg-light pb-5" style={{paddingLeft:"100px",paddingRight:"100px"}}>
+            <Container fluid className="bg-light pb-5" style={{ paddingLeft: "100px", paddingRight: "100px" }}>
                 <h2 className="py-5">Deals and Exclusive</h2>
-                <PromotionCard />
+                <Row md={1} lg={3} className="g-4">
+                    {pormotionList.map((promotion, idx) => (
+                        <Col key={idx}>
+                            <PromotionCard key={promotion.id} promotion={promotion}/>
+                        </Col>
+                    ))}
+                </Row>
             </Container>
         </div>
     )
