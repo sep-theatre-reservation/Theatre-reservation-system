4617a8dd90a0ce3c8b38609fbcd174c6dfaaaf36
ipjayawick <ipjayawick@gmail.com>
2023-09-05 22:43:07 +0530

    auth added to admin pages

--- backend/routes/carousel-router.js
+++ backend/routes/carousel-router.js
@@ -4,10 +4,11 @@ import {
   createSlide,
   deleteSlide,
 } from "../controllers/carousel-controllers.js";
+import checkAuth from "../middleware/check-auth.js";
 
 const router = express.Router();
-
 router.get("/", getSlides);
+router.use(checkAuth)
 router.post("/", createSlide);
 router.delete("/:sid", deleteSlide);
 
