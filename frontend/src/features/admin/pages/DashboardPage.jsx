914cf4ce891b2e2828543fd8b6a7e1b2bd74f3c6
Anushna Jayathunga <116999649+Anushna99@users.noreply.github.com>
2023-09-23 02:08:34 +0530

    sample charts added

--- frontend/src/features/admin/pages/DashboardPage.jsx
+++ frontend/src/features/admin/pages/DashboardPage.jsx
@@ -1,9 +1,13 @@
 import CarouselManager from "../components/CarouselManager";
+import LineChart from "../components/Charts/LineChart";
+import PieChart from "../components/Charts/PieChart";
 
 function DashboardPage() {
   return (
     <>
       <CarouselManager />
+      <LineChart />
+      <PieChart />
     </>
   );
 }
