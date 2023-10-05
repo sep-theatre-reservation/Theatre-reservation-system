20c0b342477cde7ad6368ab4e123ba08f1f6be14
Isuru Deshan <105171416+isurudeshan20@users.noreply.github.com>
2023-10-05 19:01:49 +0530

    api url config file updates

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,5 +1,6 @@
 const config = {
-    apiUrl: 'https://theatre-reservation-backend.onrender.com/api',
+    // apiUrl: 'https://theatre-reservation-backend.onrender.com/api', //uncomment for production purposes
+    apiUrl:'http://localhost:3000/api' //uncomment for development purposes
   };
   
   export default config;
