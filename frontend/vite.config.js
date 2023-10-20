3e55e1df081f5066205b4df5b48b7de09d686c3b
Anushna Jayathunga <116999649+Anushna99@users.noreply.github.com>
2023-10-20 23:47:54 +0530

    Production build removed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "../docs", // Output directory set to 'docs'
-  },
-  base: "/Theatre-reservation-system/", // Set the base path to your subfolder
 });
